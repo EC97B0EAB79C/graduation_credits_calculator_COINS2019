60a16b613428cf09de3991d8716e7c17d726f175
CHOI YEON KYU <99bannyzzz@gmail.com>
2020-08-30 14:55:04 +0900

    Implement reading data

--- GCC_COINS2019/GCC_COINS2019/GCC_COINS2019.py
+++ GCC_COINS2019/GCC_COINS2019/GCC_COINS2019.py
@@ -1,19 +1,27 @@
 import csv
 import sys
+import sqlite3
+import re
+
+
+def db_get(year, dept,courses_en):
+    gradDB=sqlite3.connect(str(year)+"/"+dept+".db")
+    cursor = gradDB.cursor()
+
 
 def f_open(fname):
     courses=[]
     courses_en=[]
     courses_ing=[]
-    test={"A+", "A", "B", "C"}
+    test={"A+", "A", "B", "C", "P"}
     with open(fname, encoding='utf-8') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
-            courses.append(row[2:])
+            courses.append(row[2:-6])
             if row[7] in test:
-                courses_en.append(row[2:])
+                courses_en.append(row[2:-6])
             elif row[7]!="D":
-                courses_ing.append(row[2:])
+                courses_ing.append(row[2:-6])
         courses.sort()
         courses_en.sort()
         courses_ing.sort()
@@ -24,7 +32,6 @@ def f_open(fname):
     return courses, courses_en, courses_ing
 
 
-
 def calculate(fname, dept, year):
     courses, courses_en, courses_ing = f_open(fname)
     print("courses taken")
@@ -33,11 +40,14 @@ def calculate(fname, dept, year):
     print("courses taking")
     for i in courses_ing:
         print(i)
+
+    db_get(year, dept,courses_en)
     
 
 def main():
     if "--fname" in sys.argv:
         fname=sys.argv[sys.argv.index("--fname")+1]
+        fname=re.sub('[&<>"]', '' , fname)
     else:
         fname="SIRS_STNB.csv"
 
@@ -48,9 +58,9 @@ def main():
 
     if "--dept" in sys.argv:
         dept=sys.argv[sys.argv.index("--dept")+1]
+        dept=re.sub('[&<>"]', '' , dept)
     else:
         dept="GB"
-
     calculate(fname, dept, year);
     
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # graduation_credits_calculator_COINS2019
- Graduation credits calculator for COINS 2019
+ Graduation credits calculator for University of Tsukuba COINS 2019
